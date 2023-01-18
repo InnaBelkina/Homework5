143ca7c7e72bf69084bd5857ffd33c07c624f961
Inna Belkina <inna0311@inbox.ru>
2023-01-18 16:19:11 +0600

    Разница между макс и мин элементами

---
+++ Task38/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 38: Задайте массив вещественных чисел. 
+// Найдите разницу между максимальным и минимальным элементов массива.
+
+// [3 7 22 2 78] -> 76
+
+int[] CreateRandomArray(int N, int start, int end)
+{
+    int[] RandomArray = new int[N];
+    for (int i = 0; i < N; i++)
+    {
+        RandomArray[i] = new Random().Next(start, end + 1);
+    }
+    return RandomArray;
+}
+
+void ShowArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+int[] myArray = CreateRandomArray(10, 1, 20);
+ShowArray(myArray);
+
+int minEl = myArray[0];
+int maxEl = myArray[0];
+
+for (int i = 0; i < myArray.Length; i++)
+{
+    if (myArray[i] > maxEl) maxEl = myArray[i];
+}
+for (int i = 0; i < myArray.Length; i++)
+{
+    if (myArray[i] < minEl) minEl = myArray[i];
+}
+
+int diff = maxEl - minEl;
+Console.WriteLine($"Разница между максимальным и минимальным значением = {diff}");
